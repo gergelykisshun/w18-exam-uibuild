7881d9ae3da00fc6314d233152538173b75d3926
GergelyKiss <gergely.kiss.hun@gmail.com>
2022-05-18 10:53:48 +0200

    book css

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import './Book.css';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const Book = ({info, num}) => {
 
   const {title, sub, text} = info
 
   return (
-    <div className='book-card'>
-      <p className='card-number'>{num + 1}</p>
-      <h3 className='book-author'>{sub}</h3>
-      <h2 className='book-title'>{title}</h2>
-      <p className='book-desc'>{text}</p>
-
+    <div className='book-wrapper'>
+      <div className='book-card'>
+        <p className='card-number'>{num + 1}</p>
+        <h3 className='book-author'>{sub}</h3>
+        <h2 className='book-title'>{title}</h2>
+        <p className='book-desc'>{text}</p>
+      </div>
+      <button className='read-more-btn'>
+        <p>read more</p>
+        <ArrowForwardIcon className="read-more-arrow"/>
+      </button>
     </div>
   )
 }
